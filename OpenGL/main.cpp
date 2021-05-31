689e63cda62c4e305c2c19afbe728899c5bc4dad
Alex <barankaslon@hotmail.com>
2021-05-31 00:05:53 -0300

    first window

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -8,6 +8,24 @@ using namespace std;
 int main() {
 	glfwInit();
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Test WindowGL", NULL, NULL);
+	if (window == NULL) {
+		cout << "Failed to create GLFW Window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
 	glfwTerminate();
 	return 0;
 }
