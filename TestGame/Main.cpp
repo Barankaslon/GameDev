ee6afadad07fd2ee480939ce8ea1ae816801e516
Alex <barankaslon@hotmail.com>
2021-06-03 21:31:05 -0300

    console whait

---
+++ TestGame/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+	using namespace sf;
+	using namespace std;
+
+int main() {
+
+	RenderWindow window(VideoMode(800, 600), "Sfml Game");
+
+	cin.get();
+
+	return 0;
+}
